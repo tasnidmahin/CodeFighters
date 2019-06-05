20eb3bd108416cb336f410419931511fa223b04a
tasnidmahin <tasnidmahin@gmail.com>
2019-06-05 13:02:08 +0600

    Login page design 1 done

---
+++ index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+	<title> Codefighters </title> 		<!-- Eta TAB e show kore-->
+	
+	
+	<meta charset="utf-8">			<!-- English sarao onno language support koranor jonno -->
+	<meta name="description"   	content="Programming Problem solving platform">
+	<meta name="keywords"		content="Codefighters, contest , problem, programming">
+	<meta name="author"			content="Tasnid Mahin">
+
+	<meta name="viewport"		content="width=device-width, initial-scale=1.0">
+	
+	
+	<link rel = "stylesheet" href="css/login_css.css">				<!-- External css file -->
+
+</head>
+
+
+<body>
+
+	<h1> <img src="gifs/logo.gif" alt="codefighters" width="582" height="130"> </h1>
+	
+	
+	<div>
+	
+		<form>
+		
+			<fieldset>
+			<legend> Login </legend>				<!-- Legend -->
+			
+			<br><br>
+			Username<br><br>
+			<input type="text"	name="username"		required><br><br>
+			Password<br><br>
+			<input type="password"	name="password"	required><br><br>
+						
+			<input type="submit"	value="Login">		<!-- Submit -->
+			<br><br>
+			
+			
+			</fieldset>
+			
+			<br>Have no account? <a href = "register.php" id="register_link"> Register </a>
+			
+		</form>
+		
+	
+	</div>
+
+
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en-US">
+
+<head>
+	<title> Codefighters </title> 		<!-- Eta TAB e show kore-->
+	
+	
+	<meta charset="utf-8">			<!-- English sarao onno language support koranor jonno -->
+	<meta name="description"   	content="Programming Problem solving platform">
+	<meta name="keywords"		content="Codefighters, contest , problem, programming">
+	<meta name="author"			content="Tasnid Mahin">
+
+	<meta name="viewport"		content="width=device-width, initial-scale=1.0">
+	
+	
+	<link rel = "stylesheet" href="css/login_css.css">				<!-- External css file -->
+
+</head>
+
+
+
+<body>
+
+	<h1> <img src="gifs/logo.gif" alt="codefighters" width="582" height="130"> </h1>
+
+	<form>
+	
+		<fieldset>
+		<legend>  Register </legend>
+		
+		
+			
+		
+		</fieldset>
+	
+	
+	</form>
+
+</body>
