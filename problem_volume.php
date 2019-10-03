e389c89fa35e68bfff11ccde07b417d8050662c8
tasnidmahin <tasnidmahin@gmail.com>
2019-10-03 11:10:20 +0600

    Database Edited
    Database Edited
    Volume Click works
    1 problem Added

--- problem_volume.php
+++ problem_volume.php
@@ -29,37 +29,98 @@
 
 	<?php
 		include("navbar.php");
+		$volume_no = $_GET['volume'];
+	 ?>
+	
+	<?php if($volume_no == null) { ?>
+		<div class="vol_tab">
+			<table align="center">
+				  <tr>
+					<th>Volume</th>
+					<th>Solved Problems</th> 
+					<th>Total Problems</th>
+				  </tr>
+				  <tr>
+					<td><a href="problem_volume.php?volume=100" >100</a></td>
+					<!-- <a href="page2.php?varname=<?php echo $var_value ?>">Page2</a> -->
+					<td>0</td>
+					<td>50</td>
+				  </tr>
+				  <tr>
+					<td><a href="problem_volume.php?volume=101">101</a></td>
+					<td>0</td>
+					<td>50</td>
+				  </tr>
+				  <tr>
+					<td><a href="problem_volume.php?volume=102">102</a></td>
+					<td>0</td>
+					<td>50</td>
+				  </tr>
+				  <tr>
+					<td><a href="problem_volume.php?volume=103">103</a></td>
+					<td>0</td>
+					<td>50</td>
+				  </tr>
+			</table>
+		</div>
+	<?php } ?>
+	
+	
+	
+	<?php if($volume_no == 100) { 
+	$sql = "SELECT ProblemID,ProblemName,"
+	
 	?>
-
-	<div class="vol_tab">
-		<table align="center">
-			  <tr>
-				<th>Volume</th>
-				<th>Solved Problems</th> 
-				<th>Total Problems</th>
-			  </tr>
-			  <tr>
-				<td><a href="#.php">100</a></td>
-				<td>0</td>
-				<td>50</td>
-			  </tr>
-			  <tr>
-				<td><a href="#.php">101</a></td>
-				<td>0</td>
-				<td>50</td>
-			  </tr>
-			  <tr>
-				<td><a href="#.php">102</a></td>
-				<td>0</td>
-				<td>50</td>
-			  </tr>
-			  <tr>
-				<td><a href="#.php">103</a></td>
-				<td>0</td>
-				<td>50</td>
-			  </tr>
-		</table>
-	</div>
+		<div class="vol_tab">
+			<table align="center">
+				  <tr>
+					<th>Problem ID</th>
+					<th>Problem Name</th> 
+					<th>Total Solve</th>
+				  </tr>
+			</table>
+		</div>
+	
+	<?php } ?>
+	
+	<?php if($volume_no == 101) { ?>
+		<div class="vol_tab">
+			<table align="center">
+				  <tr>
+					<th>Problem ID</th>
+					<th>Problem Name</th> 
+					<th>Total Solve</th>
+				  </tr>
+			</table>
+		</div>
+	
+	<?php } ?>
+	
+	<?php if($volume_no == 102) { ?>
+		<div class="vol_tab">
+			<table align="center">
+				  <tr>
+					<th>Problem ID</th>
+					<th>Problem Name</th> 
+					<th>Total Solve</th>
+				  </tr>
+			</table>
+		</div>
+	
+	<?php } ?>
+	
+	<?php if($volume_no == 103) { ?>
+		<div class="vol_tab">
+			<table align="center">
+				  <tr>
+					<th>Problem ID</th>
+					<th>Problem Name</th> 
+					<th>Total Solve</th>
+				  </tr>
+			</table>
+		</div>
+	
+	<?php } ?>
 
 
 	<script src="js/jquery.min.js"></script>
